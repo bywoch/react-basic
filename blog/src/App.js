b29b09a468864a6f4791f127a1483b8189730da4
Bright Chung <bywoch@gmail.com>
2024-08-18 16:39:21 +0900

    03_리액트에서 레이아웃 만들 때 쓰는 JSX 문법 3개 Update App.js, App.css, and README.md in blog folder

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { // ← 실은 html이 아니라 JSX임 (.js 파일에서 쓰는 html 대용품)
+  let post = '강남 우동 맛집';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* JSX 문법1. class를 넣을땐 className */}
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+
+      {/* JSX 문법2. 변수 넣을땐 {중괄호} */}
+      <div>{post}</div>
+
+      {/* JSX 문법3. style 넣을땐 style={} 
+      <div style={{ color: 'blue', fontSize: '30px' }}> 글씨 </div>
+      */}
     </div>
   );
 }
-
+/* (참고) 에러메세지는 터미널/브라우저/개발자도구에서 확인*/
 export default App;
